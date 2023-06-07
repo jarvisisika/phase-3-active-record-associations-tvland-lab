ffc14ab63a0369ac25ef82a2c1975933cdabe3e0
jarvisisika <jarvis.isika@student.moringaschool.com>
2023-06-07 14:10:18 +0300

    completed assignment

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,15 @@
 class Actor < ActiveRecord::Base
+    has_many :characters
+    has_many :shows, through: :characters
   
-end
+    def full_name
+      "#{first_name} #{last_name}"
+    end
+  
+    def list_roles
+      roles = characters.map do |character|
+        "#{character.name} - #{character.show.name}"
+      end
+    end
+  end
+  
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
-end
+    has_many :characters
+    has_many :actors , through: :characters
+    belongs_to :network
+    
+    def actors_list
+      actors.select{|show| show.full_name.first}.map(&:full_name)
+    end
+  end
